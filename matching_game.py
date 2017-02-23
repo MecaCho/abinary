f7923882693fc391d64d5c69986c83ef0dd315bb
MecaCho <1099833328@qq.com>
2017-02-23 17:49:05 +0800

    link_test

--- matching_game.py
+++ matching_game.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 #-*-coding=UTF-8
 #matching_game.py
-
 import sys, os
 import math, random
 import pygame
--- testT.py
+++ testT.py
@@ -4,12 +4,31 @@ import time
 import sys
 from pygame.color import THECOLORS
 
+
 class background(pygame.sprite.Sprite):
     def __init__(self,filename,inittial_position):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(filename)
         self.rect  = self.image.get_rect()
         self.rect.bottomright = inittial_position
+
+
+def startFlash():
+    start_location = [0,0]
+    stbg_Group = pygame.sprite.Group()
+    stbg_Group.add(background('start1.png', start_location))
+    stbg_Group.add(background('start2.png', start_location))
+    stbg_Group.add(background('start3.png', start_location))
+    for sf in stbg_Group.sprites():
+        time.sleep(3)
+        screen.blit(sf.image,sf.rect)
+
+def show_help(filename='1.png',imagelocation=[100,100]):
+    print 'True'
+    imagep = pygame.image.load(filename)
+    rect = imagep.get_rect()
+    rect.bottomright = imagelocation
+    screen.blit(imagep,rect)
 
 bg_location = ([500,300],[500,200],[500,100])
 bg_Group = pygame.sprite.Group()
@@ -39,5 +58,11 @@ while True:
             print event.pos
             clickxy = event.pos
         elif event.type==pygame.MOUSEBUTTONUP:
-            if clickxy[0]<
+            if clickxy[0]<100 and clickxy[1]<100 and clickxy[0]>0 and clickxy[1]>0:
+                show_help()
+if __name__ == "__main__":
+    game1 = match_Game()
+    game1.run()
+    game2 = link_Game()
+    game2.run()
 
